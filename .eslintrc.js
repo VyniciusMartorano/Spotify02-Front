2f9a548c2a9ee9315dc5362be634845fc01c9572
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2022-08-07 12:44:28 -0300

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: ['prettier/prettier', 'airbnb', 'plugin:prettier/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier', 'react-hooks'],
-  rules: {
-    'react/react-in-jsx-scope': 0,
-    'prettier/prettier': 0,
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'no-console': 0,
-    'func-names': 0,
-  },
+  "trailingComma": "es5",
+  "tabWidth": 4,
+  "semi": false,
+  "singleQuote": true
 };
