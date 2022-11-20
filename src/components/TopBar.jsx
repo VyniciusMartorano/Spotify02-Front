d10809ed3dfb55f5cab76f9b91b8614124ec5214
VYNICIUS MARTORANO <VyniciusMartorano>
2022-11-20 19:35:09 -0300

    estilização do finder

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,44 +1,59 @@
-import React from "react"
+import React, { Component } from "react"
 import "./../assets/css/TopBar.css"
 
 
-const TopBar = () => {
-    
+class TopBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {filter: ''}
+    }   
 
-    return (
-        <header className="top-bar-container">
-            <div className="left-side-bar">
-            </div>
-            <div className="search-container">
 
-                <div className="home-btn-box">
-                    <i className="fa-solid fa-house"></i>
+    limpaFiltro() {
+        this.setState({filter: ''})
+    }
+
+    render() {
+        return (
+            <header className="top-bar-container">
+                <div className="left-side-bar">
                 </div>
-
-                <form className="search-box">
-                    <div className="icon-search-box">
-                        <i className="fa-solid fa-magnifying-glass"></i>
+                <div className="search-container">
+    
+                    <div className="home-btn-box">
+                        <i className="fa-solid fa-house"></i>
                     </div>
-                    <input 
-                        type="text" 
-                        name="" 
-                        className="input-search"
-                        placeholder="O que você quer ouvir?"
+    
+                    <form className="search-box">
+                        <div className="icon-search-box">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </div>
+                        <input 
+                            type="text" 
+                            name="" 
+                            className="input-search"
+                            placeholder="O que você quer ouvir?"
+                            value={this.state.filter}
+                            onChange={({ target }) => this.setState({filter: target.value})}
+                        />
+                        <div className="icon-remove-text-box">
+                            {
+                                this.state.filter ? (<i onClick={() => this.limpaFiltro()} className="fa-solid fa-xmark icon-remove-text"></i>) : ''
+
+                            }
+                        </div>
+                    </form>
+                </div>
+                <div className="icon-profile-container">
+                    <img 
+                        src={require('./../utils/images/Victor 3x4-small.png')} 
+                        alt=""
+                        className="img-profile" 
                     />
-                    <div className="icon-remove-text-box">
-
-                    </div>
-                </form>
-            </div>
-            <div className="icon-profile-container">
-                <img 
-                    src={require('./../utils/images/Victor 3x4-small.png')} 
-                    alt=""
-                    className="img-profile" 
-                />
-            </div>
-        </header>
-    )
+                </div>
+            </header>
+        )
+    }
 }
 
 export default TopBar
